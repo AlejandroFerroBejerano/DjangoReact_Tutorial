0252e8e98b9fc087df5fa61c726951b39486ad44
Alejandro Julián Ferro Bejerano <ferrobejeranoalejandro@gmail.com>
2023-08-11 11:45:25 +0200

    Added basic Navigation ande Task Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { TaskPage } from "./pages/TaskPage";
+import { TaskFormPage } from "./pages/TaskFormPage";
+import { Navigation } from "./components/Navigation";
+
 function App(){
   return (
-    <div>Hello World</div>
+    <BrowserRouter>
+    <Navigation />
+
+    <Routes>
+      <Route path="/" element={<Navigate to="/Tasks"/>}/>
+      <Route path="/tasks" element={<TaskPage />}/>
+      <Route path="/tasks-create" element={<TaskFormPage />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
